df4aee9b90acb3f0aee7256d7d5d491d89e648e2
shagbag913 <sh4gbag913@gmail.com>
2018-11-12 18:28:44 -0500

    Init CornerUnrounder
    Someone on XDA wanted something like this lol
    
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- magisk-module/config.sh
+++ magisk-module/config.sh
@@ -40,9 +40,9 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "   Always On Display Enabler   "
-  ui_print "*******************************"
+  ui_print "**********************"
+  ui_print "   Corner Unrounder   "
+  ui_print "**********************"
 }
 
 ##########################################################################################
